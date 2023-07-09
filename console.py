4bcec098a854f036235bddd6c4a55ef6d3b60050
Michael Aroworade <Ayoaro85@gmail.com.com>
2023-07-08 18:23:11 -0700

    2. Console improvements

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Defines the HBNB console."""
 import cmd
 import sys
+from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
@@ -99,19 +100,41 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
-    def do_create(self, args):
-        """Usage: create <class>
-        Create a new class instance and its id.
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
         """
-        if not args:
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                new_instance = HBNBCommand.classes[my_list[0]]()
+            else:
+                new_instance = HBNBCommand.classes[my_list[0]](**kwargs)
+                storage.new(new_instance)
+            storage.save()
+            print(new_instance.id)
+            print(kwargs)
+            storage.save()
+
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
 
     def do_show(self, args):
         """Usage: show <class> <id>
