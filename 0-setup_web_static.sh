51784b93b3093a1474dbf146d6c58a3e8de4b41f
Michael Aroworade <Ayoaro85@gmail.com.com>
2023-07-12 07:55:35 -0700

    full deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,8 +19,8 @@ printf %s "server {
     root   /var/www/html;
     index  index.html index.htm;
 
-    location /hbnb_static {
-        alias /data/web_static/current;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
         index index.html index.htm;
     }
 
